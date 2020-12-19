52a7e9c95def209039e90a01bbf19c253b3c12ea
Nicholas Edwards <edwardsnjd@googlemail.com>
2020-12-19 23:00:30 +0800

    Add script to install tools on Ubuntu
    Quite hacky unfortunately.
    
    - fzf currently has an old version available via `apt`, so we install from source.
    - bat currently installs `batcat`, so we alias to `bat`
    - ripgrep currently collides with bat in install, so we install via `.deb` file

---
+++ tools/apt/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install APT packages
+cat packages.txt | xargs sudo apt install -y
+
+# FIXUP: Alias batcat to bat (old version of apt package)
+if ! [ -x "$(command -v bat)" ]; then
+  mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat ~/.local/bin/bat
+fi
+
+# WORKAROUND: Install ripgrep with flags to override installation bug
+#  See: https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517
+if ! [ -x "$(command -v rg)" ]; then
+  apt download ripgrep
+  sudo dpkg --force-overwrite -i ripgrep*.deb
+  rm -f ripgrep*.deb
+fi
+
+# WORKAROUND: Install fzf from local submodule source (to get later version)
+./fzf/install --all
