431c56109971ee2a278bfe685caab6eb3b70e68a
Nicholas Edwards <edwardsnjd@googlemail.com>
2020-12-23 23:03:04 +0800

    Improve Windows git bash experience

--- configuration/windows.sh
+++ configuration/windows.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Copy git config
-cp git/* $HOME
-cp git.windows/* $HOME
+# Copy config files to home directory
+cp -r git/. "$HOME"
+cp -r git.windows/. "$HOME"
+cp -r bash/. "$HOME"
+cp -r vim/. "$HOME"
 
 echo "TODO: Manually copy WindowsTerminal config file"
 # TODO: Copy vscode config to target
