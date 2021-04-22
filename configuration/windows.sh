b1ae59d08309b6858904ef3e0d75eaef8aa42210
Nicholas Edwards <nick.edwards@chironix.com>
2021-04-22 11:42:27 +0800

    Install bat config on windows

--- configuration/windows.sh
+++ configuration/windows.sh
@@ -4,7 +4,15 @@
 cp -r git/. "$HOME"
 cp -r git.windows/. "$HOME"
 cp -r bash/. "$HOME"
+cp -r bash.windows/. "$HOME"
 cp -r vim/. "$HOME"
+
+# NOTE: Bat config path is set in environment variable
+cp -r bat/.config/bat "$HOME"
+
+# Copy bat themes
+mkdir -p "$HOME/AppData/Roaming/bat/themes"
+cp -r bat/.config/bat/themes/. "$HOME/AppData/Roaming/bat/themes"
 
 echo "TODO: Manually copy WindowsTerminal config file"
 # TODO: Copy vscode config to target
