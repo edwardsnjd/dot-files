a7014ba91276bbd65906a466c2f32ef5a79d3f42
Nicholas Edwards <nick.edwards@chironix.com>
2021-06-21 11:24:42 +0800

    Install Github CLI (gh) on Ubuntu

--- tools/apt/install.sh
+++ tools/apt/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# WORKAROUND: Update apt sources for Github CLI tool
+#  See: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-apt
+if ! [ -f "/usr/share/keyrings/githubcli-archive-keyring.gpg" ]; then
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+    | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  sudo apt update
+fi
+
 # Install APT packages
 cat packages.txt | xargs sudo apt install -y
 
