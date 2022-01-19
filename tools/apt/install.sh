d80214722409777a733cff85439c1ca8695e53a3
Nicholas Edwards <nick.edwards@chironix.com>
2022-01-19 22:07:18 +0800

    chore: Bump bat to latest version

--- tools/apt/install.sh
+++ tools/apt/install.sh
@@ -19,7 +19,7 @@ done
 # WORKAROUND: Install bat from local binary (to get later version)
 #  See: https://github.com/sharkdp/bat/blob/master/README.md#installation
 if ! [ -x "$(command -v bat)" ]; then
-  curl -L https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb -o bat.deb
+  curl -L https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb -o bat.deb
   sudo dpkg -i bat.deb
   rm -f bat.deb
 fi
