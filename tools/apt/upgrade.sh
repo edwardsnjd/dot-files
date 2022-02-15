c02dff5bfb913510d4bf084c0de6b3484afcee34
Nicholas Edwards <nick.edwards@chironix.com>
2022-02-15 09:31:28 +0800

    chore: Correct upgrade to auto-commit submodules

--- tools/apt/upgrade.sh
+++ tools/apt/upgrade.sh
@@ -7,16 +7,19 @@ sudo apt update
 sudo apt upgrade
 
 # Upgrade vim
-(cd vim && git pull && git commit -m "chore: Bump vim to latest version")
+(cd vim && git pull)
+git add vim && git commit -em "chore: Bump vim to latest version"
 ./install-vim.sh
 
 # Upgrade fzf
-(cd fzf && git pull && git commit -m "chore: Bump fzf to latest version")
+(cd fzf && git pull)
+git add fzf && git commit -em "chore: Bump fzf to latest version"
 ./install-fzf.sh
 
 # Upgrade nb
 ./install-nb.sh
 
 # Upgrade feedgnuplot
-(cd feedgnuplot && git pull && git commit -m "chore: Bump feedgnuplot to latest version")
+(cd feedgnuplot && git pull)
+git add feedgnuplot && git commit -em "chore: Bump feedgnuplot to latest version"
 ./install-feedgnuplot.sh
