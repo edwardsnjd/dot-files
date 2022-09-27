d8856b1420fd59e7db5461de8a375ed65a60b4dd
Nicholas Edwards <nick@oculo.com.au>
2022-09-27 15:33:54 +0800

    feat: Add more bash snippets

---
+++ configuration/snippets/.config/snippets/bash/entrypoint.sh
@@ -0,0 +1,5 @@
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    <+ sourced +>
+else
+    <+ executed  +>
+fi
---
+++ configuration/snippets/.config/snippets/bash/tests.sh
@@ -0,0 +1,8 @@
+function RunTests() {
+  function test<+ +>() {
+    assertEquals <+ expected +> <+ actual +>
+  }
+
+  # Run tests
+  source shunit2
+}
