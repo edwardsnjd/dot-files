c45ec61806784de939cd5752717c4d551edd6cfc
Nicholas Edwards <nick@oculo.com.au>
2024-08-07 15:22:05 +0800

    chore: Automatically updated emacs config

--- configuration/emacs/.config/emacs/init.el
+++ configuration/emacs/.config/emacs/init.el
@@ -79,7 +79,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(night-owl-theme use-package evil)))
+ '(package-selected-packages '(night-owl-theme use-package evil))
+ '(send-mail-function 'mailclient-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
