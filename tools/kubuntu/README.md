7652f7dcb8cfbb65eea5c5ae2cb0b5cbaeb7cb2d
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-04-12 22:24:35 +0800

    feat: Add Kubuntu configuration and tooling

---
+++ tools/kubuntu/README.md
@@ -0,0 +1,15 @@
+Install all packages:
+```sh
+./install
+```
+
+Install a new package manually, then record it:
+```sh
+guix install foo
+./capture
+```
+
+Upgrade all packages:
+```sh
+./upgrade
+```
---
+++ tools/kubuntu/channels.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'edwardsnjd-guix-channel)
+        (url "https://github.com/edwardsnjd/guix-channel.git")
+        (branch "main")
+        (commit
+          "c6a19a2d43fabbf2e3f673897693b99eb41fc426"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "63f323fc98fb6e85c48721574aab8e54d95091e5")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
