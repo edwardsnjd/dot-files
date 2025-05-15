2da57a21e2cccde53da5a7b9ebf2e7e0274814e4
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-05-15 23:01:36 +0800

    feat: Add AWS SAM CLI to work tools
    Homebrew version isn't blessed by AWS so have a hardcoded install.

--- tools/homebrew-work/install.sh
+++ tools/homebrew-work/install.sh
@@ -1,3 +1,42 @@
 #!/usr/bin/env bash
 
-brew bundle install
+function Main() {
+  brew bundle install
+  AwsSamCli
+}
+
+function AwsSamCli() {
+  # See: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/reference-sam-cli-install-verify.html
+  # See: https://github.com/aws/aws-sam-cli
+
+  local packageUrl="https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-macos-arm64.pkg"
+  local packagePath="aws-sam-cli-macos-arm64.pkg"
+  local expectedFingerprint="49 68 39 4A BA 83 3B F0 CC 5E 98 3B E7 C1 72 AC 85 97 65 18 B9 4C BA 34 62 BF E9 23 76 98 C5 DA"
+
+  # Download and install
+  echo "Downloading AWS SAM CLI installer: $packageUrl"
+  curl -L "$packageUrl" --output "$packagePath"
+
+  # Verify package download
+  echo "Verifying AWS SAM CLI installer: $packageUrl"
+  pkgutil --check-signature "$packagePath" \
+    | tr -d "\n" \
+    | sed -E 's/ +/ /g' \
+    | grep -q "$expectedFingerprint" \
+    && echo "AWS SAM CLI package signatures look OK" \
+    || { echo "AWS SAM CLI package signatures look BAD!"; exit 1; }
+
+  # Install
+  echo "Installing AWS SAM CLI: $packageUrl"
+  sudo installer -pkg "$packagePath" -target "/"
+
+  # Clean up
+  echo "Deleting AWS SAM CLI installer: $packagePath"
+  rm "$packagePath"
+
+  # Confirm installed ok
+  which sam
+  sam --version
+}
+
+Main "$@"
