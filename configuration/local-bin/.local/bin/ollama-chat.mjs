689e160ae168cccbb398c17301b9184eecc5fb52
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-10-16 00:09:47 +0800

    feat: Add slash commands to llm chat, including images
    - /dump = json dump of chat for debugging
    - /file = with tab completion for file to include
    - /image = with tab completion for image to include

--- configuration/local-bin/.local/bin/ollama-chat.mjs
+++ configuration/local-bin/.local/bin/ollama-chat.mjs
@@ -1,11 +1,17 @@
 #! /usr/bin/env node
 
 import { createInterface } from 'readline'
+import { readFileSync, readdirSync } from 'fs'
+import * as path from 'path'
 
 const OLLAMA_HOST = 'http://127.0.0.1:11434'
-const END_OF_INPUT_MARKER = '.'
-
 const DEFAULT_MODEL = 'llama3.2'
+
+const END_OF_INPUT_MARKER = '.'
+const CLEAR_COMMAND = '/clear'
+const DUMP_COMMAND = '/dump'
+const FILE_COMMAND = '/file '
+const IMAGE_COMMAND = '/image '
 
 // OLLAMA: Requests
 
@@ -16,10 +22,10 @@ const buildChat = (model, messages = [], format = '', options = {}) => ({
   options,
 })
 
-const buildMessage = (content, role) => ({ content, role })
+const buildMessage = (content, role, extras = {}) => ({ content, role, ...extras })
 
-const appendUserMessage = (chat, content) =>
-  appendMessage(chat, buildMessage(content, 'user'))
+const appendUserMessage = (chat, content, images) =>
+  appendMessage(chat, buildMessage(content, 'user', { images }))
 
 const appendAssistantMessage = (chat, content) =>
   appendMessage(chat, buildMessage(content, 'assistant'))
@@ -32,8 +38,48 @@ const appendMessage = (chat, message) => ({
 // CLI
 
 const buildPrompt = (input, output) => {
-  const rl = createInterface({ input, output })
+  const rl = createInterface({ input, output, completer })
   return (query) => new Promise((resolve) => rl.question(query, resolve))
+}
+
+function completer(line) {
+  if (line.length === 0 || line === '/') return [
+    [CLEAR_COMMAND, DUMP_COMMAND, FILE_COMMAND, IMAGE_COMMAND],
+    line,
+  ]
+
+  if (CLEAR_COMMAND.startsWith(line)) return [[CLEAR_COMMAND], line]
+  if (DUMP_COMMAND.startsWith(line)) return [[DUMP_COMMAND], line]
+  if (FILE_COMMAND.startsWith(line)) return [[FILE_COMMAND], line]
+  if (IMAGE_COMMAND.startsWith(line)) return [[IMAGE_COMMAND], line]
+
+  if (line.startsWith(FILE_COMMAND)) {
+    const fileInput = line.substring(FILE_COMMAND.length)
+    return [(filesMatching(fileInput)).map(r => `${FILE_COMMAND}${r}`), line]
+  }
+
+  if (line.startsWith(IMAGE_COMMAND)) {
+    const fileInput = line.substring(IMAGE_COMMAND.length)
+    return [(filesMatching(fileInput)).map(r => `${IMAGE_COMMAND}${r}`), line]
+  }
+
+  return [[], line]
+}
+
+function filesMatching(line) {
+  const input = line.trim()
+  const [dir, prefix] = input.endsWith(path.sep)
+    ? [input, '']
+    : [path.dirname(input), path.basename(input)]
+
+  try {
+    return readdirSync(path.resolve(dir), { withFileTypes: true })
+      .filter(entry => entry.name.startsWith(prefix))
+      .map(entry => entry.isDirectory() ? `${entry.name}${path.sep}` : entry.name)
+      .map(entry => path.join(dir, entry))
+  } catch (e) {
+    return []
+  }
 }
 
 // HTTP
@@ -86,8 +132,8 @@ async function start(model) {
 
   const resetChat = () => { chat = buildChat(model) }
 
-  const handleUserInput = async (userContent) => {
-    chat = appendUserMessage(chat, userContent)
+  const handleUserInput = async (userContent, images) => {
+    chat = appendUserMessage(chat, userContent, images)
 
     const httpResponse = await send(chat)
 
@@ -108,33 +154,55 @@ async function start(model) {
 
   const getUserInput = async () => {
     const lines = []
+    const images = []
     while (true) {
       const line = await prompt('')
       if (line == END_OF_INPUT_MARKER) break
-      lines.push(line)
+      if (line.startsWith(FILE_COMMAND)) {
+        const filePath = line.substring(FILE_COMMAND.length).trim()
+        lines.push(filePath)
+        lines.push(readFileSync(filePath, 'utf-8'))
+        console.log(`(added file ${filePath})`)
+      } else if (line.startsWith(IMAGE_COMMAND)) {
+        const filePath = line.substring(IMAGE_COMMAND.length).trim()
+        images.push(readFileSync(filePath).toString('base64'))
+        console.log(`(added image ${filePath})`)
+      } else {
+        lines.push(line)
+      }
     }
-    return lines.join('\n')
+    return { lines, images }
   }
 
   mainLoop:
   while (true) {
     console.log('*🤔 User*')
-    const userContent = await getUserInput()
+    const { lines, images } = await getUserInput()
+
+    const userContent = lines.join('\n')
 
     switch (userContent) {
       case 'exit':
         console.log("(exiting)")
         break mainLoop
 
-      case '/clear':
+      case CLEAR_COMMAND:
         resetChat()
         console.log("(chat has been reset)")
         console.log()
         break
 
+      case DUMP_COMMAND:
+        console.log("(dumping chat)")
+        console.log('```json')
+        console.log(JSON.stringify(chat, null, 2))
+        console.log('```')
+        console.log()
+        break
+
       default:
         console.log('*🤖 Assistant*')
-        await handleUserInput(userContent)
+        await handleUserInput(userContent, images)
         console.log() // don't overwrite response
         console.log()
     }
