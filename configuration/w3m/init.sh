47335f610fb8f96db7404b986f9669d80ffe7ed2
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-10-24 22:09:24 +0800

    chore: Ensure success on first configuration run
    The first run no `.bash_profile` is in place yet to set the `XDG_CONFIG_HOME`
    environment variable so the script errors.  This change makes sure that it gets
    set to a sensible default.

--- configuration/w3m/init.sh
+++ configuration/w3m/init.sh
@@ -5,4 +5,5 @@
 # of them under XDG_CONFIG_HOME.  By ensuring that directory exists we
 # make sure that stow does not symlink the entire thing and instead mixes
 # in our configuration files alongside the runtime files.
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "$XDG_CONFIG_HOME/.w3m"
